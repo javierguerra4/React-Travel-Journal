5397e8bc8292561207321e717211c67eb2be3319
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-29 02:03:27 -0500

    Centered content in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,8 +4,8 @@ export default function Navbar() {
   return (
     <nav>
       <div className="navbar--content">
-        <i class="fa fa-globe fa-2x" aria-hidden="true"></i>
-        <h2>My Travel Journal</h2>
+        <i class="fa fa-globe fa-3x" aria-hidden="true"></i>
+        <h2 className="navbar--header">My Travel Journal</h2>
       </div>
     </nav>
   );
