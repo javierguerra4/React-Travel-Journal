7f5d95e2d3570885cc4266d3376df9d63cc92393
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-29 01:43:30 -0500

    Centered content in Navbar in center

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
+import React from "react";
 import "./App.css";
+import Navbar from "./Navbar";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="navbar--content">
+        <i class="fa fa-globe fa-2x" aria-hidden="true"></i>
+        <h2>My Travel Journal</h2>
+      </div>
+    </nav>
+  );
+}
