32546ca56e9a6a2f5698763cb7f8831ebae4bd30
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-31 11:42:33 -0500

    first line on card is flexed

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,14 +7,20 @@ export default function Card(props) {
         <img src={props.item.imageUrl} className="card--image"></img>
       </a>
       <div className="card--content">
-        <p className="card--location">
-          <i class="fa-solid fa-location-dot"></i>
-          {props.item.location}
-        </p>
-        <a href={props.item.googleMapsUrl} target="_blank">
-          {" "}
-          View on Google Maps
-        </a>
+        <div className="card--line--one">
+          <p className="card--location">
+            <i class="fa-solid fa-location-dot"></i>
+            {props.item.location}
+          </p>
+          <a
+            href={props.item.googleMapsUrl}
+            target="_blank"
+            className="google--link"
+          >
+            {" "}
+            View on Google Maps
+          </a>
+        </div>
         <h1 className="card--title">{props.item.title}</h1>
         <h4 className="card--date">{props.item.startDate}</h4>
         <p className="card--description">{props.item.description}</p>
