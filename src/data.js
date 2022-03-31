42a273542fd998728c3dd0033043f4523975d045
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-30 23:34:52 -0500

    Updated data.js with new imageUrl

--- src/data.js
+++ src/data.js
@@ -8,6 +8,7 @@ export default [
     endDate: "March, 2021",
     description:
       "The Narrows is the narrowest section of Zion Canyon in Zion National Park, Utah, United States. Situated on the North Fork of the Virgin River and upstream of the main canyon, The Narrows is one of the premier hikes in the park and on the Colorado Plateau.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    imageUrl:
+      "https://images.unsplash.com/photo-1608762335563-d4dd4919ce11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80",
   },
 ];
