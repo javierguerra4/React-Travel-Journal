9950268b3a9bf927437d314022b7c65faa0c155c
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-31 12:14:56 -0500

    Card isnow in center

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,28 +2,28 @@ import React from "react";
 
 export default function Card(props) {
   return (
-    <div className="card">
-      <a href={props.item.imageUrl} target="_blank">
-        <img src={props.item.imageUrl} className="card--image"></img>
-      </a>
-      <div className="card--content">
-        <div className="card--line--one">
-          <p className="card--location">
+    <div className="card--whole">
+      <div className="card">
+        <a href={props.item.imageUrl} target="_blank">
+          <img src={props.item.imageUrl} className="card--image"></img>
+        </a>
+        <div className="card--content">
+          <div className="card--line--one">
             <i class="fa-solid fa-location-dot"></i>
-            {props.item.location}
-          </p>
-          <a
-            href={props.item.googleMapsUrl}
-            target="_blank"
-            className="google--link"
-          >
-            {" "}
-            View on Google Maps
-          </a>
+            <p className="card--location">{props.item.location}</p>
+            <a
+              href={props.item.googleMapsUrl}
+              target="_blank"
+              className="google--link"
+            >
+              {" "}
+              View on Google Maps
+            </a>
+          </div>
+          <h1 className="card--title">{props.item.title}</h1>
+          <h4 className="card--date">{props.item.startDate}</h4>
+          <p className="card--description">{props.item.description}</p>
         </div>
-        <h1 className="card--title">{props.item.title}</h1>
-        <h4 className="card--date">{props.item.startDate}</h4>
-        <p className="card--description">{props.item.description}</p>
       </div>
       <hr></hr>
     </div>
